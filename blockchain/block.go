da3e1ebacbbd2d2b50a402903d903eac65e27508
rishabh <shahrishabh1904@gmail.com>
2025-12-15 00:51:48 +0530

    added badger db

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,14 +1,22 @@
 package blockchain
 
-type BlockChain struct {
-	Blocks []*Block
-}
+import (
+	"bytes"
+	"encoding/gob"
+	"log"
+)
 
 type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
 	Nonce    int
+}
+
+func Handle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
@@ -22,16 +30,24 @@ func CreateBlock(data string, prevHash []byte) *Block {
 	return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-	prevBlock := chain.Blocks[len(chain.Blocks)-1]
-	newBlock := CreateBlock(data, prevBlock.Hash)
-	chain.Blocks = append(chain.Blocks, newBlock)
-}
-
 func Genesis() *Block {
 	return CreateBlock("Genesis", []byte{})
 }
 
-func InitBlockChain() *BlockChain {
-	return &BlockChain{[]*Block{Genesis()}}
+func (b *Block) Serialize() []byte {
+	var res bytes.Buffer
+	encoder := gob.NewEncoder(&res)
+
+	err := encoder.Encode(b)
+
+	Handle(err)
+	return res.Bytes()
+}
+
+func Deserialize(data []byte) *Block {
+	var block Block
+	decoder := gob.NewDecoder(bytes.NewReader(data))
+	err := decoder.Decode(&block)
+	Handle(err)
+	return &block
 }
--- main.go
+++ main.go
@@ -1,24 +1,97 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"go_chain/blockchain"
+	"os"
+	"runtime"
 	"strconv"
+
+	// Replace with your actual module path
+	"go_chain/blockchain"
 )
 
-func main() {
-	chain := blockchain.InitBlockChain()
-	chain.AddBlock("First block after genesis")
-	chain.AddBlock("Second block after genesis")
-	chain.AddBlock("Third block after genesis")
+type CLI struct {
+	chain *blockchain.BlockChain
+}
 
-	for _, block := range chain.Blocks {
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Data %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
+func (cli *CLI) printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println(" add -block <BLOCK_DATA> - Add a block to the chain")
+	fmt.Println(" print - Prints the blocks in the chain")
+}
 
-		pow := blockchain.NewProof(block)
-		fmt.Printf("PoW: %s \n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
+func (cli *CLI) validateArgs() {
+	if len(os.Args) < 2 {
+		cli.printUsage()
+		runtime.Goexit()
 	}
+}
+
+func (cli *CLI) addBlock(data string) {
+	cli.chain.AddBlock(data)
+	fmt.Println("Success!")
+}
+
+func (cli *CLI) printChain() {
+	iter := cli.chain.Iterator()
+
+	for {
+		block := iter.Next()
+
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := blockchain.NewProof(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
+		fmt.Println()
+
+		if len(block.PrevHash) == 0 {
+			break
+		}
+	}
+}
+
+func (cli *CLI) Run() {
+	cli.validateArgs()
+
+	addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+
+	addBlockData := addBlockCmd.String("block", "", "Block data")
+
+	switch os.Args[1] {
+	case "add":
+		err := addBlockCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+	case "print":
+		err := printChainCmd.Parse(os.Args[2:])
+		blockchain.Handle(err)
+	default:
+		cli.printUsage()
+		runtime.Goexit()
+	}
+
+	if addBlockCmd.Parsed() {
+		if *addBlockData == "" {
+			addBlockCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.addBlock(*addBlockData)
+	}
+
+	if printChainCmd.Parsed() {
+		cli.printChain()
+	}
+}
+
+func main() {
+	// Proper exit handling for BadgerDB
+	defer os.Exit(0)
+
+	chain := blockchain.InitBlockChain()
+	defer chain.Database.Close()
+
+	cli := CLI{chain}
+	cli.Run()
 }
